0018955414f1e47fed34e47eb40afcda0f67c945
rober0xf <robertinopili13@gmail.com>
2025-01-04 15:33:57 -0300

    800: watermelon

---
+++ codeforces/800/watermelon/watermelon.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// input 1 <= w <= 100
+using namespace std;
+int main() {
+    int w;
+
+    cin >> w;
+
+    if (w % 2 == 0 && w > 2) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+
+    return 0;
+}
