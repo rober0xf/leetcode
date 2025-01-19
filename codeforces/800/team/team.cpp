9a949f1da5203ca6b1486ee7f2d623767dfe5f89
rober0xf <robertinopili13@gmail.com>
2025-01-19 19:09:14 -0300

    800: teams

---
+++ codeforces/800/team/team.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int count = 0;
+    for (int i = 0; i < n; ++i) {
+        int petya, vasya, tonya;
+        cin >> petya >> vasya >> tonya;
+
+        if (petya + vasya + tonya >= 2)
+            ++count;
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
